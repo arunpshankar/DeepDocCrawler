a8de8a56ada3f156e4aed1a57f78b40f93a8a56a
arunpshankar <arunprshankar@gmail.com>
2023-10-18 09:54:18 -0400

    wip

--- tests/test_config.py
+++
@@ -1,7 +0,0 @@
-from src.config.logging import setup_logger
-from src.config.setup import load_config
-
-logger = setup_logger()
-
-config = load_config('./config/config.yml')
-logger.info(config)
--- tests/test_crawler.py
+++
@@ -1,7 +0,0 @@
-from src.crawl.crawler import DeepCrawler
-
-crawler = DeepCrawler()
-
-# Test crawling a sample URL
-urls = crawler.crawl("https://example1.com")
-print("Crawled URLs:", urls)
