d2f02eb6d00c9b58b65ef3784b2a4141cf9a6eb8
arunpshankar <arunprshankar@gmail.com>
2023-10-18 10:03:18 -0400

    wip

--- src/main.py
+++ src/main.py
@@ -1,17 +1,14 @@
-from config.setup import load_config
-from crawl.crawler import WebCrawler
-from mine.miner import PDFMiner
 from orchestrate.orchestrator import LLMOrchestrator
+from crawl.crawler import WebCrawler
+from config.setup import load_config
+from mine.miner import PDFMiner
 import jsonlines
 import asyncio
 
 
-DEPTH = 20
+DEPTH = 5
 
 def main():
-    config = load_config('./config/config.yml')
-    
-    data = []
     with jsonlines.open('./config/sites.jsonl') as sites:
         for site in sites:
             print(site)
@@ -20,5 +17,5 @@ def main():
             crawler = WebCrawler(base_url=url, depth=DEPTH, company=company)
             asyncio.run(crawler.crawl())
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
