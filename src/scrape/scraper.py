342a3b37c5858aca2e1ab9624f74118596255a96
arunpshankar <arunprshankar@gmail.com>
2023-10-18 23:36:44 -0400

    wip

--- src/scrape/scraper.py
+++ src/scrape/scraper.py
@@ -76,7 +76,7 @@ class WebScraper:
 
         for retry in range(max_retries + 1):  # +1 to account for the first try
             logger.info(f'Scraping URL: {input_url} (Attempt: {retry + 1})')
-            timeout = ClientTimeout(total=1)  # Set to 1 second
+            timeout = ClientTimeout(total=10)  # Set to 10 seconds
 
             try:
                 async with aiohttp.ClientSession() as session:
